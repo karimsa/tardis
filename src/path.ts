d7f559889656e4836fd9c8e3023b092193174ba6
Karim Alibhai <karim@foko.co>
2018-03-01 23:47:47 -0500

    Add better node verification

--- src/path.ts
+++ src/path.ts
@@ -5,12 +5,33 @@
 
 import { Node } from './types'
 
+function verifyNode(node: Node) {
+  if (typeof node.type !== 'string' || !node.type) {
+    throw new Error('Node is missing a valid type')
+  }
+
+  if (typeof node.children !== 'function') {
+    throw new Error(`Invalid node of type ${node.type}: missing a valid children function`)
+  }
+
+  if (typeof node.toString !== 'function' || node.toString === {}.toString) {
+    node.toString = () => {
+      throw new Error(
+        `Invalid node of type ${node.type}: missing a valid toString() function - will not generate`
+      )
+    }
+  }
+}
+
 export class Path {
   constructor (
     public node: Node,
-  ) {}
+  ) {
+    verifyNode(node)
+  }
 
   replaceWith (node: Node) {
+    verifyNode(node)
     this.node = node
   }
 }
