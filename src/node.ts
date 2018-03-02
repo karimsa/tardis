1a3e9379a43fcc3fe356a4a79cee0b29234b1180
Karim Alibhai <karim@foko.co>
2018-03-01 23:47:33 -0500

    Add visitor and walker merging

--- src/node.ts
+++ src/node.ts
@@ -4,12 +4,51 @@
  */
 
 import { Path } from './path'
-import { SyncWalker, AsyncWalker } from './types'
-
-export type JSON = {
-  [key: string]: any,
-}
+import { JSON, Walker, AsyncWalker } from './types'
 
 export type Visitor = {
-  [key: string]: SyncWalker | AsyncWalker,
+  [key: string]: Walker,
+}
+
+function runWalker (walker: Walker, path: Path, state?: JSON): Promise<void> {
+  if (<AsyncWalker>walker) {
+    return (<AsyncWalker>walker)(path, state)
+  }
+
+  return new Promise(function syncWalker (resolve, reject) {
+    try {
+      resolve(walker(path, state))
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+export function mergeWalkers (... walkers: Walker[]): Walker {
+  return function mergedWalker (path: Path, state?: JSON): Promise<void> {
+    return Promise.all(walkers.map(function (walker) {
+      return runWalker(walker, path, state)
+    })).then(function () {
+      // discard any value from coming down
+      return
+    })
+  }
+}
+
+export function mergeVisitors (... visitors: Visitor[]): Visitor {
+  const visitor: Visitor = {}
+
+  for (const mergableVisitor of visitors) {
+    for (const nodeType in mergableVisitor) {
+      if (mergableVisitor.hasOwnProperty(nodeType)) {
+        if (visitor[nodeType]) {
+          visitor[nodeType] = mergeWalkers(visitor[nodeType], mergableVisitor[nodeType])
+        } else {
+          visitor[nodeType] = mergableVisitor[nodeType]
+        }
+      }
+    }
+  }
+
+  return visitor
 }
